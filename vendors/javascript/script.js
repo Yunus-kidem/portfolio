1a4f904332b8979b65be362255ee3817d8acaefe
unknown <yunuskt363@gmail.com>
2021-12-12 21:00:01 +0300

    project

---
+++ vendors/javascript/script.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if (this.scrollY > 20) {
+            $('.navbar').addClass("sticky");
+        } else {
+            $('.navbar').removeClass("sticky");
+        }
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function() {
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+});
+
+/* For the sticky navigation */
+$('.js--section-features').waypoint(function(direction) {
+    if (direction == "down") {
+        $('nav').addClass('sticky');
+    } else {
+        $('nav').removeClass('sticky');
+    }
+}, {
+    offset: '60px;'
+});
+
+
+/* Scroll on buttons */
+$('.js--scroll-to-plans').click(function() {
+    $('html, body').animate({ scrollTop: $('.js--section-plans').offset().top }, 1000);
+});
+
+$('.js--scroll-to-start').click(function() {
+    $('html, body').animate({ scrollTop: $('.js--section-features').offset().top }, 1000);
+});
+
+
+/* Navigation scroll */
+$(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
